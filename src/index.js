773d55d792f3016a9c488f163af8ed088df7d3c4
DelaSonia <dela.sonia58@gmail.com>
2024-07-05 10:04:45 +0000

    adding Readme files

---
+++ src/index.js
@@ -0,0 +1,23 @@
+let Heading=document.getElementById("Heading");
+Heading.style.background= 'blue'
+Heading.style.color= 'white'
+
+let DOM=document.getElementById("DOM");
+DOM.style.backgroundColor= 'rgb(0, 132, 255);'
+DOM.innerHTML =  "Document Object Module"
+
+let Access=document.getElementById("Access");
+Access.style.backgroundColor= 'aqua'
+Access.innerHTML =  "I know how to Access Elements In A DOM"
+
+let Elements=document.getElementById("Elements");
+Elements.style.backgroundColor= 'aquamarine'
+Elements.innerHTML =  "I Know How To Use The Style Methods To Change The Style Of An Element In A DOM"
+
+document.querySelector("Styling");
+Styling.style.backgroundColor= 'rgb(106, 28, 233)'
+Styling.innerHTML =  "I Know How The Styling Properties Differ "
+
+document.querySelector("Excited");
+Excited.style.backgroundColor= 'rgb(50, 28, 233)'
+Excited.innerHTML =  "Yes I did it, i manipulated the DOM and styled my Elements"
